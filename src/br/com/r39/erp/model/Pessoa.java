ca55378e1e3a56061e0467c522490e34e74c4d34
tiagokazuolino <tiagokazuolino@NTB-T430-TKL>
2017-05-11 22:16:42 -0300

    Insert Entity Annotation

--- src/br/com/r39/erp/model/Pessoa.java
+++ src/br/com/r39/erp/model/Pessoa.java
@@ -1,8 +1,15 @@
 package br.com.r39.erp.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Pessoa {
-	
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer Id;
+	
 	private String Nome;
 	private String Email;
 	private String Observacao;
