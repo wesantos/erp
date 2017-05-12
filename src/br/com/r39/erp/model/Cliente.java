0c624984b89f4f7f36e58679095719db6c3e0679
tiagokazuolino <tiagokazuolino@NTB-T430-TKL>
2017-05-11 21:28:26 -0300

    Update mode Cliente;

--- src/br/com/r39/erp/model/Cliente.java
+++ src/br/com/r39/erp/model/Cliente.java
@@ -1,5 +1,40 @@
 package br.com.r39.erp.model;
 
 public class Cliente {
-
+	private int Id;
+	private String Nome;
+	private String LatLong;
+	private String Endereco;
+	private String Observacao;
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	public String getLatLong() {
+		return LatLong;
+	}
+	public void setLatLong(String latLong) {
+		LatLong = latLong;
+	}
+	public String getEndereco() {
+		return Endereco;
+	}
+	public void setEndereco(String endereco) {
+		Endereco = endereco;
+	}
+	public String getObservacao() {
+		return Observacao;
+	}
+	public void setObservacao(String observacao) {
+		Observacao = observacao;
+	}
+	
 }
