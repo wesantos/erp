5a577f9253e9d1f127603762d54b82f2e5dc89fd
Wesley Santos <Wesley Santos@DESKTOP-9N28DRK>
2017-05-11 21:20:09 -0300

    Create a model Pessoa.

---
+++ src/br/com/r39/erp/model/Pessoa.java
@@ -0,0 +1,37 @@
+package br.com.r39.erp.model;
+
+public class Pessoa {
+	
+	private int Id;
+	private String Nome;
+	private String Email;
+	private String Observacao;
+	
+	
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getObservacao() {
+		return Observacao;
+	}
+	public void setObservacao(String observacao) {
+		Observacao = observacao;
+	}
+	
+
+}
